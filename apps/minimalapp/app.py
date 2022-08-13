1e48d6c15e4322cd7669784d2c0d205b12032578
futabato <01futabato10@gmail.com>
2022-08-13 10:13:04 +0900

    :sparkles: テンプレートエンジンを使ってHTMLをレンダリングした

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/names/{name}", response_class=HTMLResponse)
+async def read_item(request: Request, name: str):
+    return templates.TemplateResponse("name.html", {"request": request, "name": name})
