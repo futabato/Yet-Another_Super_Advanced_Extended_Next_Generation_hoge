ae8da9559eb08588c7aac8253a97ff01ccedd76e
futabato <01futabato10@gmail.com>
2022-08-14 15:21:10 +0900

    :sparkles: request contextを完全に理解した

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -14,3 +14,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/names/{name}", response_class=HTMLResponse)
 async def read_item(request: Request, name: str):
     return templates.TemplateResponse("name.html", {"request": request, "name": name})
+
+@app.get("/items/{item_id}")
+def read_root(item_id: str, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "item_id": item_id}
