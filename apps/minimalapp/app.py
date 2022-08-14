1547aa6e809044359bef0bf9269c17b2dfa9907b
futabato <01futabato10@gmail.com>
2022-08-15 00:01:54 +0900

    :sparkles: 簡単にPRGパターンで問い合わせフォームを作った

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -10,12 +10,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/contact")
+def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
 
-@app.get("/names/{name}", response_class=HTMLResponse)
-async def read_item(request: Request, name: str):
-    return templates.TemplateResponse("name.html", {"request": request, "name": name})
-
-@app.get("/items/{item_id}")
-def read_root(item_id: str, request: Request):
-    client_host = request.client.host
-    return {"client_host": client_host, "item_id": item_id}
+@app.get("/contact/complete", response_class=HTMLResponse)
+@app.post("/contact/complete", response_class=RedirectResponse)
+def contact_complete(request: Request):
+    if request.method == "POST":
+        # メールを送る
+        
+        # contact endpointへリダイレクトする
+        return RedirectResponse("/contact/complete", status_code=303)
+    return templates.TemplateResponse("contact_complete.html", {"request": request})
